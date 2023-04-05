62d49c5d3146a3a0b966385c786b3b2533c04eeb
il060 <il06.03.77@gmail.com>
2023-04-05 16:04:32 +0500

    Create echo bot and create config.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+import logging
+from telegram.ext import Application, MessageHandler, filters
+from config import BOT_TOKEN
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def echo(update, context):
+    await update.message.reply_text(update.message.text)
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    text_handler = MessageHandler(filters.TEXT, echo)
+    application.add_handler(text_handler)
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
